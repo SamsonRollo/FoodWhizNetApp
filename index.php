317925b4ea417c422d64a98c55e2e84297639208
SamsonRollo <sdrollo@up.edu.ph>
2024-04-18 10:31:37 +0800

    Add css

--- index.php
+++ index.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Image Upload</title>
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
-    <h2>Upload Image</h2>
     <form action="upload.php" method="post" enctype="multipart/form-data">
+        <h2>Upload Image</h2>
         <input type="file" name="image" accept="image/*">
         <input type="submit" value="Upload">
     </form>
